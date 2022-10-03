9210e8f60fd43dee4d9bc28036d96c7f77bb7335
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 <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>
2022-10-03 10:02:05 -0500

    Unit selector needs refinement.

--- PageSizeSelector.py
+++ PageSizeSelector.py
@@ -5,6 +5,12 @@
 
 import json
 import PySimpleGUI as sg
+from os import system
+
+
+def clear():
+    """This function only has support to clear the screen for Linux of Mac."""
+    _ = system("clear")
 
 
 def load_paper_data(standards = '', formats = '', units = ''):
@@ -13,16 +19,6 @@ def load_paper_data(standards = '', formats = '', units = ''):
     with open('all_paper_sizes.json') as file_paper_sizes:
         data_paper_sizes = json.load(file_paper_sizes)
 
-    # Report to console recieved parameters
-    if standards != '':
-        print('\n-------------\n  SELECTION \n-------------')
-        print('load_paper_data():')
-        print('> standards = {}'.format(standards))
-        print('> formats = {}'.format(formats))
-        print('> units = {}'.format(units))
-        # print('> sizes = {}'.format(paper_data['paper_sizes']))
-        print('-------------')
-
     # Dictionary for returning values of paper selections
     paper_data = {
         "paper_standards": [],
@@ -30,6 +26,16 @@ def load_paper_data(standards = '', formats = '', units = ''):
         "paper_units": [],
         "paper_sizes": [],
     }
+
+    # # Report to console recieved parameters
+    # if standards != '':
+    #     print('\n-------------\n  SELECTION \n-------------')
+    #     print('load_paper_data():')
+    #     print('> standards = {}'.format(standards))
+    #     print('> formats = {}'.format(formats))
+    #     print('> units = {}'.format(units))
+    #     # print('> sizes = {}'.format(paper_data['paper_sizes']))
+    #     print('-------------')
 
     # Populate "paper_standards".
     # Return dictionary with "paper_standards"
@@ -53,23 +59,44 @@ def load_paper_data(standards = '', formats = '', units = ''):
 
     if formats != '':
         for units, void in data_paper_sizes[standards][formats].items():
+            # print(units)
             paper_data['paper_units'].append(units)
         # for sizes, void in data_paper_sizes[standards][formats][units]:
+            # print(sizes)
+            # paper_data['paper_sizes'].append(sizes)
+
+        # for sizes, void in data_paper_sizes[standards][formats][units]:
+        # paper_data['paper_sizes'] = data_paper_sizes[standards][formats][0]
         paper_data['paper_sizes'] = data_paper_sizes[standards][formats][units]
+        print(data_paper_sizes[standards][formats][units])
+
+
+        # if units == 'mm':
+        #     units = 0
+        # elif units == 'points':
+        #     units = 1
+        # elif units == 'inches':
+        #     units = 2
 
         print('\n-------------')
         print('> units = {}'.format(paper_data['paper_units']))
         print('> sizes = {}'.format(paper_data['paper_sizes']))
+        print('> sizes = {}'.format(paper_data['paper_sizes'][0]))
+        # print('> sizes = {}'.format(paper_data['paper_sizes'][units))
+
         print('-------------')
         # return paper_data
 
-    if units != '':
-        # paper_data['paper_sizes'] = data_paper_sizes[standards][formats][units].items()
-        paper_data['paper_sizes'] = data_paper_sizes[standards][formats][units]
-        print('\n-------------')
-        print('> sizes = {}'.format(paper_data['paper_sizes']))
-        # print('> dimension = {}'.format(paper_data['paper_sizes']))
-        print('-------------')
+    # if units != '':
+    #     for sizes, void in data_paper_sizes[standards][formats].items():
+    #         # print(sizes)
+    #         paper_data['paper_units'].append(sizes)
+    #     # paper_data['paper_sizes'] = data_paper_sizes[standards][formats][units].items()
+    #     # paper_data['paper_sizes'] = data_paper_sizes[standards][formats][sizes]
+    #     print('\n-------------')
+    #     print('> sizes = {}'.format(paper_data['paper_sizes']))
+    #     # print('> dimension = {}'.format(paper_data['paper_sizes']))
+    #     print('-------------')
 
     return paper_data
 
@@ -84,10 +111,15 @@ def draw_window(paper_data = None):
     }
 
     layout = [
+       [sg.Text('paper_standards', key='-PAPER_STANDARDS-')],
        [sg.Combo(list(paper_data["paper_standards"]), **options, key="paper_standards")],
+       [sg.Text('paper_formats', key='-PAPER_FORMATS-')],
        [sg.Combo((), **options, key="paper_formats")],
+       [sg.Text('paper_units', key='-PAPER_UNITS-')],
        [sg.Combo((), **options, key="paper_units")],
-       [sg.Text('DIMENSTION', key='-DIMENSION-')],
+       [sg.Text('paper_size', key='-PAPER_SIZE-')],
+       [sg.Text('0 X 0', key='-PAPER_DIMENSION-'),
+        sg.Text(paper_data['paper_units'])]
        # [sg.Combo((), **options, key="paper_sizes")],
        # [sg.InputText(default_text = 'Default text', key='_DIMENSION_BOX_')],
        # [sg.Text()],
@@ -96,17 +128,17 @@ def draw_window(paper_data = None):
     return sg.Window('Page Size Selector', layout, keep_on_top=True)
 
 
-def  evaluate_window(window = None):
+def  evaluate_window(window = None, paper_data = None):
     while True:
 
         event, values = window.read()
-        print('\n-------------')
-        print('> event = {}'.format(event))
-        print('> values[paper_standards] = {}'.format(values['paper_standards']))
-        print('> values[paper_formats] = {}'.format(values['paper_formats']))
-        print('> values[paper_units] = {}'.format(values['paper_units']))
-        # print('> values[{}] = {}'.format(event, values[event]))
-        print('-------------')
+        # print('\n-------------')
+        # print('> event = {}'.format(event))
+        # print('> values[paper_standards] = {}'.format(values['paper_standards']))
+        # print('> values[paper_formats] = {}'.format(values['paper_formats']))
+        # print('> values[paper_units] = {}'.format(values['paper_units']))
+        # # print('> values[{}] = {}'.format(event, values[event]))
+        # print('-------------')
 
         if event == sg.WINDOW_CLOSED:
             break
@@ -120,19 +152,20 @@ def  evaluate_window(window = None):
             seletion_paper_format = values[event]
             paper_data = load_paper_data(standards = seletion_paper_standard, formats = seletion_paper_format, units = None)
             window['paper_units'].Update(value = paper_data['paper_units'][0], values = paper_data['paper_units'])
+            # window['paper_units'].Update(value = paper_data['paper_units'][0], values = paper_data['paper_units'])
             # window['paper_units'].Update(value = 'test', values = 'tests')
 
         elif event == 'paper_units':
             seletion_paper_unit = values[event]
-            print('\n-------------')
-            print('paper_units: {}'.format(values[event]))
-            print('-------------')
             paper_data = load_paper_data(standards = seletion_paper_standard, formats = seletion_paper_format, units = seletion_paper_unit)
             paper_size = str(paper_data['paper_sizes'][0]) + ' x ' + str(paper_data['paper_sizes'][1])
-            print('\n-------------')
-            print('paper_size: {}'.format(paper_size))
-            print('-------------')            # window['paper_sizes'].Update(value = paper_size, values = paper_data['paper_sizes'])
-            window['-DIMENSION-'].update(paper_size)
+            window['-PAPER_DIMENSION-'].update(paper_size)
+            # print('\n-------------')
+            # print('paper_units: {}'.format(values[event]))
+            # print('-------------')
+            # print('\n-------------')
+            # print('paper_size: {}'.format(paper_size))
+            # print('-------------')            # window['paper_sizes'].Update(value = paper_size, values = paper_data['paper_sizes'])
             # window.Element('_DIMENSION_BOX_').update(default_text = values)
         # elif event == "paper_sizes":
         #     lst = paper_data[values[event]]
@@ -142,6 +175,9 @@ def  evaluate_window(window = None):
 
 def main():
 
+    # Empty the console screen.
+    clear()
+
     # Create paper_data object for summoning paper sizes.
     paper_data = load_paper_data()
 
@@ -149,7 +185,7 @@ def main():
     PaperSizeSelector_window = draw_window(paper_data)
 
     # Evaluate the window actions.
-    evaluate_window(PaperSizeSelector_window)
+    evaluate_window(PaperSizeSelector_window, paper_data)
 
 # Starting from terminal
 if __name__ == "__main__":
